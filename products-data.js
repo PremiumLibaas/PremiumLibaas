959da4d7bb3221638303eb76384d3dabab70985b
Premium Libaas <premiumlibaas@gmail.com>
2026-01-04 01:45:43 +0000

    Add files via upload

---
+++ products-data.js
@@ -0,0 +1,29 @@
+/* =========================
+   PRODUCT DATA (ONLY)
+========================= */
+
+const products = [
+  {
+    id: "THB-0001",
+    category: "thobe",
+    subcategory: "men",
+    title: "Classic Thobe",
+    subtitle: "Premium Fabric",
+    image: "thobe1.jpeg",
+    extraImages: ["thobe1-2.jpeg"],
+    oldPrice: "£39.99",
+    price: "£25.00",
+    },
+  {
+    id: "ABY-0001",
+    category: "abayah",
+    subcategory: "luxury",
+    title: "Elegant Abayah",
+    subtitle: "Flowing Silhouette",
+    image: "abayah1.webp",
+    extraImages: ["abayah1-2.webp"],
+    oldPrice: "",
+    price: "£35.00",
+    
+  }
+];
