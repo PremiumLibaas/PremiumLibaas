653a1e175ae3553874832863dcfeb9ecfd7e8a40
Premium Libaas <premiumlibaas@gmail.com>
2026-01-04 02:41:13 +0000

    Update products-data.js

--- products-data.js
+++ products-data.js
@@ -26,16 +26,6 @@ const products = [
     price: "£35.00",
     
   },
-  {
-    id: "nce-0001",
-    category: "nice",
-    subcategory: "hot",
-    title: "Elegant Abayah",
-    subtitle: "Flowing Silhouette",
-    image: "abayah1.webp",
-    extraImages: ["abayah1-2.webp"],
-    oldPrice: "",
-    price: "£35.00",
-    
-  },
+
 ];
+
