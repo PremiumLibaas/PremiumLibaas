edf6b945b6b2c4914893472ee35a06ffc27db9a8
Premium Libaas <premiumlibaas@gmail.com>
2026-01-07 23:14:01 +0000

    Update products-data.js

--- products-data.js
+++ products-data.js
@@ -10,7 +10,7 @@ const products = [
     title: "Classic Thobe",
     subtitle: "Premium Fabric",
     image: "thobe1.jpeg",
-    extraImages: ["thobe1-2.jpeg"],
+    extraImages: ["thobe1-2.jpeg", "thobe1-3.webp"],
     oldPrice: "£39.99",
     price: "£25.00",
     },
